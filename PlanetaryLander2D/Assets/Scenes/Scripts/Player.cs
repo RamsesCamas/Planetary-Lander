220ddd0768fa9734766883f3c01569b7e3c0e52b
Ramses Camas <machiniram@gmail.com>
2021-10-02 16:29:24 -0500

    Physics up and right added, gravity similar to The Moon

---
+++ PlanetaryLander2D/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    private void FixedUpdate(){
+        if(Input.GetAxis("Vertical")>00.1f){
+            rb.AddForce(transform.up * 30.0f);
+        }
+        if(Input.GetAxis("Horizontal")>00.1f){
+            rb.AddForce(transform.right * 10.0f);
+        }
+    }
+}
