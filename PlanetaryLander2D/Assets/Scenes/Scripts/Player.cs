74d92c662459364f3eb804b739d314859918ccd9
Ramses Camas <machiniram@gmail.com>
2021-10-02 20:30:56 -0500

    Added gravity from Mars, Europa and the Sun

--- PlanetaryLander2D/Assets/Scenes/Scripts/Player.cs
+++ PlanetaryLander2D/Assets/Scenes/Scripts/Player.cs
@@ -6,8 +6,12 @@ public class Player : MonoBehaviour
 {
     public Rigidbody rb;
     // Start is called before the first frame update
+    private Vector3 v_thrust_earth_gravity; 
+
+    private float gravityValue = 9.807f;
+
     void Start(){
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,13 +20,35 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate(){
+        rb.useGravity = false;
+        rb.AddForce(new Vector3(0,-1.0f,0)*rb.mass*gravityValue);
         //rb.AddForce(transform.up * 10.0f); Equivale a la gravedad de la tierra
         if(Input.GetAxis("Vertical")>00.1f){
-            Vector3 v_thrust_earth_gravity = transform.up * 10.0f;
+            v_thrust_earth_gravity = transform.up * 11.0f;
             rb.AddForce(v_thrust_earth_gravity);
         }
-        if(Input.GetAxis("Horizontal")>00.1f){
-            rb.AddForce(transform.right * 5.0f);
+        if(Input.GetAxis("Horizontal")>00.1f || Input.GetAxis("Horizontal") < 00.1f ){
+            rb.AddForce(transform.right * 5.0f * Input.GetAxis("Horizontal"));
         }
     }
+
+    public void LunarGravity(){
+        gravityValue = 1.62f;
+    }
+
+    public void MarsGravity(){
+        gravityValue = 3.721f;
+    }
+
+    public void VenusGravity(){
+        gravityValue = 8.87f;
+    }
+
+    public void EuropaGravity(){
+        gravityValue = 1.315f;
+    }
+    public void SunGravity(){
+        gravityValue = 274f;
+    }
+
 }
